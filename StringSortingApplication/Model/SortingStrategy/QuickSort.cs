4538290b0c8f708487a24436f83768d776959127
Walter R. Robledo <robleo.walterrobbin@gmail.com>
2020-08-13 17:14:39 +0800

    Added Quick Sort Algorithm

--- StringSortingApplication/Model/SortingStrategy/QuickSort.cs
+++ StringSortingApplication/Model/SortingStrategy/QuickSort.cs
@@ -14,7 +14,48 @@ namespace StringSortingApplication.Model.SortingStrategy
 
         public string Execute(string str)
         {
-            return "This is QuickSort Return Value";
+            char[] ArrayStr = StringSortingService.ConvertToCharacterArray(str);
+            int low = 0;
+            int high = ArrayStr.Length-1;
+
+            QuickSortAlgo(ArrayStr, low, high);
+
+            return StringSortingService.ConvertCharArrayToString(ArrayStr);
+        }
+
+        private void QuickSortAlgo(char[] arrayStr, int low, int high)
+        {
+            if(low < high)
+            {
+                int pi = Partition(arrayStr, low, high);
+
+                QuickSortAlgo(arrayStr, low, pi - 1);
+                QuickSortAlgo(arrayStr, pi + 1, high);
+            }
+        }
+
+        private int Partition(char[] arrayStr, int low, int high)
+        {
+            char Pivot = arrayStr[high];
+
+            int i = (low - 1);
+            for (int j = low; j< high; j++)
+            {
+                if(arrayStr[j] < Pivot)
+                {
+                    i++;
+
+                    char temp = arrayStr[i];
+                    arrayStr[i] = arrayStr[j];
+                    arrayStr[j] = temp;
+                }
+            }
+
+            char temp2 = arrayStr[i + 1];
+            arrayStr[i + 1] = arrayStr[high];
+            arrayStr[high] = temp2;
+
+            return i + 1;
         }
     }
 }
