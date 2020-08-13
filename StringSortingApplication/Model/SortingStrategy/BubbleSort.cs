e4a7c5271f01c9da9e272e9529b45dd783fc7033
Walter R. Robledo <robleo.walterrobbin@gmail.com>
2020-08-13 15:52:12 +0800

    Finished BubbleSort Algorithm

--- StringSortingApplication/Model/SortingStrategy/BubbleSort.cs
+++ StringSortingApplication/Model/SortingStrategy/BubbleSort.cs
@@ -14,7 +14,26 @@ namespace StringSortingApplication.Model.SortingStrategy
 
         public string Execute(string str)
         {
-            return "This is BubbleSort Return Value";
+            char[] ArrayStr = StringSortingService.ConvertToCharacterArray(str);
+
+            char temp;
+            for (int j = 0; j <= ArrayStr.Length-2; j++) {
+                for(int i = 0; i <= ArrayStr.Length -2; i++)
+                {
+                    if(ArrayStr[i] > ArrayStr[i + 1])
+                    {
+                        temp = ArrayStr[i + 1];
+                        ArrayStr[i + 1] = ArrayStr[i];
+                        ArrayStr[i] = temp;
+                    }
+                }
+
+            }
+
+            return StringSortingService.ConvertCharArrayToString(ArrayStr);
         }
+
+      
+       
     }
 }
---
+++ StringSortingApplication/Model/StringSortingService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSortingApplication.Model
+{
+    public static class StringSortingService
+    {
+        public static char[] ConvertToCharacterArray(string Str)
+        {
+            char[] tmp;
+
+            tmp = Str.ToCharArray();
+
+            return tmp;
+        }
+
+        public static string ConvertCharArrayToString(char[] charArray)
+        {
+            StringBuilder stringBuilder = new StringBuilder("");
+           
+
+           for(int i = 0; i < charArray.Length; i++)
+            {
+                stringBuilder.Insert(i, charArray[i]);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
